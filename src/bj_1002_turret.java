4eadccfd3534c6bf7e97f3332929d833ab088b0d
youthdonut <dbtmdgus14@naver.com>
2025-04-07 12:00:14 +0900

    알고리즘 1

--- src/bj_1002_turret.java
+++ src/bj_1002_turret.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class turret {
+public class bj_1002_turret {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
